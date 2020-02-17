1cf76d7c82f1efce7d7ce5de902474ce273a1256
QianFan <Chakracore.net@outlook.com>
2020-02-17 17:28:34 +0800

    code cleanup

--- src/VirusSimulator.Processor/PersonMoveProcessor.cs
+++ src/VirusSimulator.Processor/PersonMoveProcessor.cs
@@ -36,29 +36,7 @@ namespace VirusSimulator.Processor
 				 }
 			 });
 
-			//context.Persons.ForAllParallel((int index,ref PositionItem p) =>
-			//{
-			//	MoveStatus m = context.MoveStatus.Items.Span[p.ID];
-			//	if (m.IsMovingToTarget==MovingStatusEnum.Moving)
-			//	{
-			//		Vector2 currentTarget = m.CurrentTarget;
-			//		if (Vector2.DistanceSquared(p.Position, currentTarget) <=d)
-			//		{
-			//			//target reached
-			//			p.MoveTo(currentTarget);
-			//			context.MoveStatus.Update(p.ID, (ref MoveStatus ms) =>
-			//			 {
-			//				 ms.IsMovingToTarget = MovingStatusEnum.Idle;
-			//			 });
-			//		}
-			//		else
-			//		{
-			//			Vector2 direction = Vector2.Normalize(currentTarget - p.Position);
-			//			Vector2 newPos = p.Position + direction * Speed;
-			//			p.MoveTo(newPos);
-			//		}
-			//	}
-			//});
+			
 		}
 	}
 }
